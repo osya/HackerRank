0d74ce7b3ed288eb2ddb050dd4b6029464dd95a2
Valeriy Osipov <info@vosipov.com>
2015-07-08 16:56:15 +0300

    Решил задачу Sherlock and The Beast

---
+++ Algorithms/Implementation/SherlockAndTheBeast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var t = int.Parse(Console.ReadLine());
+        int[] nArr = new int[t];
+        for (int i = 0; i < t; i++)
+        {
+            nArr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < t; i++)
+        {
+            var res = "-1";
+
+            for (int j = 0; j <= nArr[i]/5; j++)
+            {
+                if ((nArr[i] - j*5)%3 == 0)
+                {
+                    res = new String('5', nArr[i] - j*5) + new String('3', j*5);
+                    break;
+                }
+            }
+            Console.WriteLine(res);
+        }
+
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
