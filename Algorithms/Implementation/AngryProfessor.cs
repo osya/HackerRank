0adb54f5af581f88eb5499c710f88821de1f94b2
Valeriy Osipov <info@vosipov.com>
2015-07-08 17:08:02 +0300

    Solved task Angry Professor

---
+++ Algorithms/Implementation/AngryProfessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var t = int.Parse(Console.ReadLine());
+        int[] n = new int[t];
+        int[] k = new int[t];
+        int[][] time = new int[t][];
+        for (int i = 0; i < t; i++)
+        {
+            var line = Console.ReadLine().Split(' ');
+            n[i] = int.Parse(line[0]);
+            k[i] = int.Parse(line[1]);
+            time[i] = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        }
+        for (int i = 0; i < t; i++)
+        {
+            var c = 0;
+            for (int j = 0; j < n[i]; j++)
+            {
+                if (time[i][j] <= 0)
+                {
+                    c++;
+                }
+            }
+            if (c < k[i])
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
